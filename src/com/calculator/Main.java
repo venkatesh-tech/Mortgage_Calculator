068b9476af786bf7c9b56b0be28fad23874da574
Venkateshe <kolapallinagavenkatesh@gmail.com>
2022-12-11 14:05:03 +0530

    Refactoring Repetitive patterns

--- src/com/calculator/Main.java
+++ src/com/calculator/Main.java
@@ -6,39 +6,54 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		int principal = 0;
-		float annualInterest = 0;
-		byte years = 0;
+//		int principal = 0;
+//		float annualInterest = 0;
+//		byte years = 0;
 
 		Scanner scanner = new Scanner(System.in);
 
+		int principal = (int) readNumber("Principal", 1000, 1_000_000);
+//		while (true) {
+//			System.out.print("Principal: ");
+//			principal = scanner.nextInt();
+//			if (principal >= 1000 && principal <= 1_000_000)
+//				break;
+//			System.out.println("Enter a value between 1000 and 1000000 ");
+//		}
+
+		float annualInterest = (float) readNumber("Annual Interest Rate:", 1, 30);
+//		while (true) {
+//			System.out.print("Annual Interest Rate: ");
+//			annualInterest = scanner.nextFloat(); // Always use descriptive and meaningful names
+//			if (annualInterest >= 1 && annualInterest <= 30)
+//				break;
+//			System.out.println("Enter a value between 0 and 30");
+//		}
+
+		byte years = (byte) readNumber("Period (Years): ", 1, 30);
+//		while (true) {
+//			System.out.print("Period (Years): ");
+//			years = scanner.nextByte();
+//			if (years >= 1 && years <= 30)
+//				break;
+//			System.out.println("Enter a value between 1 and 30");
+//		}
+
+		calculateMortgage(principal, annualInterest, years);
+
+	}
+
+	public static double readNumber(String prompt, double min, double max) {
+		Scanner scanner = new Scanner(System.in);
+		double value;
 		while (true) {
-			System.out.print("Principal: ");
-			principal = scanner.nextInt();
-			if (principal >= 1000 && principal <= 1_000_000)
+			System.out.print(prompt);
+			value = scanner.nextFloat(); // Always use descriptive and meaningful names
+			if (value >= min && value <= max)
 				break;
-			System.out.println("Enter a value between 1000 and 1000000 ");
+			System.out.println("Enter a value between " + min + " and " + max + "");
 		}
-
-		while (true) {
-			System.out.print("Annual Interest Rate: ");
-			annualInterest = scanner.nextFloat(); // Always use descriptive and meaningful names
-			if (annualInterest >= 1 && annualInterest <= 30)
-				break;
-
-			System.out.println("Enter a value between 0 and 30");
-		}
-
-		while (true) {
-			System.out.print("Period (Years): ");
-			years = scanner.nextByte();
-			if (years >= 1 && years <= 30)
-				break;
-			System.out.println("Enter a value between 1 and 30");
-		}
-
-		Main.calculateMortgage(principal, annualInterest, years);
-
+		return value;
 	}
 
 	public static double calculateMortgage(int principal, float annualInterest, byte years) {
