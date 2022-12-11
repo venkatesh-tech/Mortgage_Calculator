e55882413294c030b6aed7e1523f6af9ec946e37
Venkateshe <kolapallinagavenkatesh@gmail.com>
2022-12-11 15:07:11 +0530

    Added Refactoring

--- src/com/calculator/Main.java
+++ src/com/calculator/Main.java
@@ -14,23 +14,28 @@ public class Main {
 		float annualInterest = (float) readNumber("Annual Interest Rate: ", 1, 30);
 		byte years = (byte) readNumber("Period (Years): ", 1, 30);
 
-		double mortgage = calculateMortgage(principal, annualInterest, years);
-		String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+		printMortgage(principal, annualInterest, years);
 
-		System.out.println();
-		System.out.println("MORTGAGE");
-		System.out.println("------------");
+		printPaymentSchedule(principal, annualInterest, years);
+	}
 
-		System.out.println("Monthly Payments: " + mortgageFormatted);
-
+	private static void printPaymentSchedule(int principal, float annualInterest, byte years) {
 		System.out.println();
 		System.out.println("PAYMENT SCHEDULE");
 		System.out.println("------------");
-
 		for (short month = 1; month <= years * MONTHS_IN_YEAR; month++) {
 			double balance = calculateBalance(principal, annualInterest, years, month);
 			System.out.println(NumberFormat.getCurrencyInstance().format(balance));
 		}
+	}
+
+	private static void printMortgage(int principal, float annualInterest, byte years) {
+		double mortgage = calculateMortgage(principal, annualInterest, years);
+		String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+		System.out.println();
+		System.out.println("MORTGAGE");
+		System.out.println("------------");
+		System.out.println("Monthly Payments: " + mortgageFormatted);
 	}
 
 	public static double readNumber(String prompt, double min, double max) {
